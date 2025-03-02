e554753832fd9f4a5f0ca3da813237250009c591
Shubham Gupta <shubhamgupta.srg@gmail.com>
2025-03-02 23:30:31 +0530

    Description
    - Added more projects
    - Added filters in projects
    - Other minor changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -49,3 +49,57 @@ document.addEventListener("DOMContentLoaded", function () {
 
         animateNumber("client-number", 40, 2000); // Animate to 40+ in 2 seconds
         animateNumber("project-number", 55, 2000); // Animate to 55+ in 2 seconds
+
+
+        /*FILTER PROJECT*/
+        function toggleAll() {
+            let allCheckbox = document.getElementById("all");
+            let checkboxes = document.querySelectorAll(".filter-bar input[type='checkbox']:not(#all)");
+
+            if (allCheckbox.checked) {
+                // Delay unchecking all checkboxes
+                checkboxes.forEach((cb, index) => {
+                    setTimeout(() => cb.checked = false, index * 100); // 100ms delay per checkbox
+                });
+            }
+            setTimeout(filterProjects, checkboxes.length * 100); // Apply filter after delay
+        }
+
+        function filterProjects() {
+            let allCheckbox = document.getElementById("all");
+            let checkboxes = document.querySelectorAll(".filter-bar input[type='checkbox']:not(#all)");
+            let selectedCategories = [];
+
+            checkboxes.forEach(cb => {
+                if (cb.checked) {
+                    selectedCategories.push(cb.value);
+                }
+            });
+
+            // If all checkboxes are selected, tick "All" and untick others with delay
+            if (selectedCategories.length === checkboxes.length) {
+                setTimeout(() => {
+                    allCheckbox.checked = true;
+                    checkboxes.forEach((cb, index) => {
+                        setTimeout(() => cb.checked = false, index * 100);
+                    });
+                }, 100);
+                selectedCategories = [];
+            } else {
+                allCheckbox.checked = false;
+            }
+
+            // If no category is selected, show all projects and check "All" with a delay
+            if (selectedCategories.length === 0) {
+                setTimeout(() => allCheckbox.checked = true, 100);
+            }
+
+            let projects = document.querySelectorAll(".project");
+
+            projects.forEach((project, index) => {
+                let isVisible = selectedCategories.some(category => project.classList.contains(category));
+                setTimeout(() => {
+                    project.style.display = (selectedCategories.length === 0 || isVisible) ? "block" : "none";
+                }, index * 0); // Staggered effect
+            });
+        }
